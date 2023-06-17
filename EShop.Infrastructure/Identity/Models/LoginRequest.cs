59bbf3801867e07876392e29b65578a01a60736b
Sami KHWAIREH <sami.khwaireh@std.izu.edu.tr>
2023-06-17 13:47:27 +0300

    add datavalidation

--- EShop.Infrastructure/Identity/Models/LoginRequest.cs
+++ EShop.Infrastructure/Identity/Models/LoginRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace EShop.Infrastructure.Identity.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Infrastructure.Identity.Models;
 
 public class LoginRequest
 {
+    [Required]
     public string Username { get; set; }
+    [Required]
     public string Password { get; set; }
 }
--- EShop.Infrastructure/Identity/Models/RegisterRequest.cs
+++ EShop.Infrastructure/Identity/Models/RegisterRequest.cs
@@ -1,10 +1,18 @@
-﻿namespace EShop.Infrastructure.Identity.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Infrastructure.Identity.Models;
 
 public class RegisterRequest
 {
+    [Required]
     public string Username { get; set; }
+    [Required]
     public string Password { get; set; }
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
 }
